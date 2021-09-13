46d308c7f45706123a39b5a6b9483eb07140b96f
Jefferson S Coelho <jefferson.coelho@globalhitss.com.br>
2021-09-13 19:07:28 -0300

    add create update

--- src/main/java/br/com/global/springwebmvc/model/Jedi.java
+++ src/main/java/br/com/global/springwebmvc/model/Jedi.java
@@ -1,9 +1,6 @@
 package br.com.global.springwebmvc.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 
 
--- src/main/java/br/com/global/springwebmvc/rest/JediResource.java
+++ src/main/java/br/com/global/springwebmvc/rest/JediResource.java
@@ -4,11 +4,14 @@ package br.com.global.springwebmvc.rest;
 import br.com.global.springwebmvc.model.Jedi;
 import br.com.global.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class JediResource {
@@ -22,8 +25,14 @@ public class JediResource {
     }
 
     @GetMapping("/api/jedi/{id}")
-    public Jedi findById(@PathVariable("id")  Long id){
-        return repository.findById(id).get();
+    public ResponseEntity<Jedi> findById(@PathVariable("id")  Long id){
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        if(jedi.isPresent()){
+            return ResponseEntity.ok(jedi.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/api/jedi/name/{name}")
@@ -34,6 +43,31 @@ public class JediResource {
     @GetMapping("/api/jedi/lastname/{lastname}")
     public List<Jedi> findByLastName(@PathVariable("lastname")  String lastname){
         return repository.findByLastName(lastname);
+    }
+
+    @PostMapping("/api/jedi")
+    public ResponseEntity<Jedi>  Create(@Valid @RequestBody Jedi jedi) throws URISyntaxException {
+        final Jedi jediSaved = repository.save(jedi);
+        URI uri = new URI(String.format("http://localhost:8080/api/jedi/%d",jediSaved.getId()));
+
+        return ResponseEntity.created(uri).body(jediSaved);
+    }
+
+    @PutMapping("/api/jedi/{id}")
+    public ResponseEntity<Jedi> updateJedi(@PathVariable("id") Long id, @Valid @RequestBody Jedi jedi ){
+
+        final Optional<Jedi> jediOptional = repository.findById(id);
+
+        if(jediOptional.isEmpty()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        final Jedi jediUpdated = jediOptional.get();
+
+        jediUpdated.setName(jedi.getName());
+        jediUpdated.setLastName(jedi.getLastName());
+
+        return ResponseEntity.ok(repository.save(jediUpdated));
     }
 
 
